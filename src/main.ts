a5147f8e80d02a60b50713e996c441a7de4c9cfd
Gabriel-Cz <gabrielczhz@gmail.com>
2023-02-03 18:46:50 -0600

    FUNC  (dev) Extending main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.ORIGIN || '*',
+    methods: 'OPTIONS,GET,HEAD,PUT,POST,DELETE',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    })
+  );
+  const config = new DocumentBuilder()
+    .setTitle('LMS API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
